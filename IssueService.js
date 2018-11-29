234fe1e19b90700caf3df09492cfad7d120ea196
Adrián de la Torre Rodríguez <atr96@correo.ugr.es>
2018-11-29 18:11:49 +0100

    Quito el bot del programa closes #22

--- IssueService.js
+++ IssueService.js
@@ -76,8 +76,6 @@ app.get('/see_issues/:id', function (req, res) {
 
 
 
-
-
 //Start the app
 var server = app.listen(app.get('port'), function () {
   console.log('App listening on port ' + app.get('port'));
@@ -85,70 +83,3 @@ var server = app.listen(app.get('port'), function () {
 });
 
 module.exports = server
-
-
-//============== Bot part ==============
-
-//API Token Telegram
-const token = '768646003:AAEcUjONl0oSFCpP-b66YD0-sbOpd30qxsw';
-
-//Create a bot
-const TeleBot = require('telebot')
-const bot = new TeleBot(token);
-//const request = require('request');
-
-
-//start function
-bot.on('/start', (msg) => msg.reply.text("Hi! I'm IssueBot. If you need help type: /help"));
-
-//help function
-bot.on('/help', (msg) => msg.reply.text("Commands: \n /add <description of the issue> to add a new issue \n/see to see all the issues \n/delete <issue_id> to delete the issue"));
-
-
-//Function to save issues
-bot.on(/^\/add (.+)$/, (msg, props) => {
-
-	if(props.match[1] != ""){
-		iss.addIssue(msg.chat.id, props.match[1]);
-		//Send confirmation
-		return bot.sendMessage(msg.chat.id, "Issue added.", { replyToMessage: msg.message_is } );
-	}
-	else {
-		logger.warn("No issue added for the chat id " + msg.chat.id);
-	}
-
-});
-
-//Function to see the issues
-bot.on('/see', (msg) => {
-
-
-	//Get the issues
-	var issues = iss.getIssues(msg.chat.id);
-	if(issues.length == 0)
-		logger.info("No issues for the chat id " + msg.chat.id);
-
-	for(i=0; i<issues.length; i++)
-		bot.sendMessage(msg.chat.id, '#' + i + ' ' + issues[i], { replyToMessage: msg.message_is } );
-
-
-});
-
-
-//Function to delete the issue
-bot.on(/^\/delete (.+)$/, (msg, props) =>{
-	var iss_id = parseInt(props.match[1]);
-
-
-	if(iss_id+1){	//To make sure tha it is a number. (+ 1) to make the value 0 true.
-		iss.deleteIssue(msg.chat.id, iss_id);
-		bot.sendMessage(msg.chat.id, "Issue #" + iss_id + " deleted.", { replyToMessage: msg.message_is } );
-	}
-	else{
-		bot.sendMessage(msg.chat.id, "Use: /delete <issue_id>", { replyToMessage: msg.message_is } );
-		logger.warn("No issue was deleted for chat id " + msg.chat.id);
-	}
-});
-
-
-bot.start();
