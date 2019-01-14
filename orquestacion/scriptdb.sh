8332bb43bcd2f45d5e9972c5a8b880ab9ee07880
Adrián de la Torre Rodríguez <atr96@correo.ugr.es>
2019-01-14 19:15:47 +0100

    Prueba orquestación #43

--- orquestacion/scriptdb.sh
+++ orquestacion/scriptdb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Script para la provisión de la máquina virtual de la base de datos
 
+#Variables para la version de mongo
+codename=xenial
+mongodb=3.6
+
 # Instalamos git
 echo "Instalando git..."
 sudo apt-get install git -y
@@ -9,11 +13,16 @@ sudo apt-get install git -y
 echo "Clonando repositorio..."
 sudo git clone https://github.com/adritake/CC_UGR_Personal.git
 
+
+echo "Añadiendo repositorio de mongodb..."
+wget -qO- https://www.mongodb.org/static/pgp/server-${mongodb}.asc | sudo apt-key add
+sudo bash -c "echo deb http://repo.mongodb.org/apt/ubuntu ${codename}/mongodb-org/$mongodb multiverse > /etc/apt/sources.list.d/mongodb-org.list"
+
 echo "Actualizando paquetes..."
 sudo apt-get update -y
 
 echo "Instalando mongodb..."
-sudo apt install -y mongodb
+sudo apt-get install -y mongodb-org
 
 # Es necesario activar ufw para poder abrir puertos posteriormente. --force para que no sea interactivo
 echo "Activando cortafuegos..."
@@ -23,10 +32,14 @@ sudo ufw --force enable
 echo "Abriendo puerto 27017..."
 sudo ufw allow 27017
 
+# Como hemos activado el cortafuegos es necesario permitir el puerto 80
+echo "Abriendo puerto 80..."
+sudo ufw allow 27017
+
 # Copiamos el archivo customizado de configuración de mongo
 echo "Moviendo archivo de configuración de mongo..."
-sudo mv CC_UGR_Personal/orquestacion/mongodb.conf /etc/
+sudo mv CC_UGR_Personal/orquestacion/mongod.conf /etc/
 
 # Reiniciamos mongo para que surja efecto la configuración nueva
 echo "Reiniciando mongodb..."
-sudo service mongodb restart
+sudo service mongod restart
